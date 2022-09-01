1b75d93914ea63c4e9aaa714017490ffae407e8f
RyanNazario0357 <Ryan0357@icloud.com>
2022-09-01 18:48:49 -0400

    created startquestions section for index file

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ const addBlankLine = () => {
     console.log(` \n`)
 }
 
+const startQuestion = () => {
+    addBlankLine();
+    inquirer.createPromptModule({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What is going to be done?',
+        choices: [
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee',
+            'View Employees',
+            'Add Role',
+            'Remove Role',
+            'View Roles',
+            'Add Department',
+            'Remove Department',
+            'View Departments',
+            'End'
+        ]
+    })
+}
