44f3b5858a6f25aa4823cb036491829167948d8c
RyanNazario0357 <Ryan0357@icloud.com>
2022-09-01 18:41:21 -0400

    started code for index file

---
+++ index.js
@@ -0,0 +1,9 @@
+const connection = require('./db/connection');
+const inquirer = require('inquirer');
+const figlet = require('figlet');
+const consoleTable = require('console.table');
+
+const addBlankLine = () => {
+    console.log(` \n`)
+}
+
