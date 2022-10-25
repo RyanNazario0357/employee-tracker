a0f01405b6e2c5d153edef8bc6a9cc6a0b93b619
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-25 19:19:48 -0400

    finished db and started schema

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY_KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
