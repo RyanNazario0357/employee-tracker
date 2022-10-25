2135095caaddb9328300a9b50f30387113cb0c07
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-25 19:27:24 -0400

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,19 @@ CREATE TABLE roles (
     REFERENCES departments(id)
     ON DELETE SET NULL
 );
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_roles
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    CONSTRAINT fk_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
