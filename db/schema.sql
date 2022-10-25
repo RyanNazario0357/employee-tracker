41af27a16c2df3e3db8e7db426e85c520bfc408d
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-25 19:22:54 -0400

    created table roles:

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,17 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY_KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6,0) NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_departments
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
