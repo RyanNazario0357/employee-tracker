a84608f48c2b48c42af471ced9fd17b2977fe278
RyanNazario0357 <Ryan0357@icloud.com>
2022-10-25 20:03:01 -0400

    created insert into roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,13 @@ VALUE
 ('Finance'),
 ('Legal');
 
+INSERT INTO roles (title, slaary, department_id)
+VALUE
+('Sales Lead', 100000, 1),
+('Salesperson', 70000, 1),
+('Lead Engineer', 120000, 2),
+('Software Engineer', 140000, 2),
+('Account Manager', 180000, 3), 
+('Accountant', 145000, 3),
+('Legal Team Lead', 255000, 4),
+('Lawyer', 200000, 4);
